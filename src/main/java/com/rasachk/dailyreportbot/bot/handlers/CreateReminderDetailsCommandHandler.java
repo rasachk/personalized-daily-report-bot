14bc666c621365774fec4e9b819b0a53ffe1c7de
Rasa Chekeni <chekani.rasa@gmail.com>
2026-02-11 19:34:43 +0330

    Feat: V-0.8.0 Handled Weather Forcast Reminder Detail Button

--- src/main/java/com/rasachk/dailyreportbot/bot/handlers/CreateReminderDetailsCommandHandler.java
+++ src/main/java/com/rasachk/dailyreportbot/bot/handlers/CreateReminderDetailsCommandHandler.java
@@ -1,10 +1,20 @@
 package com.rasachk.dailyreportbot.bot.handlers;
 
+import com.rasachk.dailyreportbot.config.Constants;
+import com.rasachk.dailyreportbot.reminder.model.ReminderType;
+import com.rasachk.dailyreportbot.user.model.SessionState;
 import com.rasachk.dailyreportbot.user.service.TelegramUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -14,6 +24,62 @@ public class CreateReminderDetailsCommandHandler implements CommandHandler {
 
     @Override
     public SendMessage handle(Update update) {
-        return null;
+        Map<String, String> parameters = telegramUserService.getUserSessionParameters(update.getMessage().getFrom());
+
+        ReminderType reminderType = ReminderType.fromTitle(parameters.get(Constants.TYPE_KEY));
+
+        SendMessage sendMessage;
+        switch (reminderType) {
+            case WEATHER_FORCAST -> sendMessage = handleWeatherForcastDetailButton(update, parameters);
+//            case CURRENCY -> sendMessage = handleCurrencyTypeButton(update);
+//            case PERSONAL -> sendMessage = handlePersonalTypeButton(update);
+//            case SPORTS -> sendMessage = handleSportsTypeButton(update);
+            default ->
+                    sendMessage = new SendMessage(String.valueOf(update.getMessage().getChatId()), Constants.COMMAND_NOT_RECOGNIZED_ERROR);
+        }
+
+        return sendMessage;
+    }
+
+
+    private SendMessage handleWeatherForcastDetailButton(Update update, Map<String, String> parameters) {
+        //TODO VALIDATE LOCATION
+        parameters.put(Constants.LOCATION_KEY, update.getMessage().getText());
+        telegramUserService.updateUserSessionState(update.getMessage().getFrom(), SessionState.CREATE_REMINDER_TIME, parameters);
+        return generateReminderTimeSendMessage(update.getMessage().getChatId());
+    }
+
+
+    private SendMessage generateReminderTimeSendMessage(Long chatId) {
+
+        SendMessage sendMessage = new SendMessage(String.valueOf(chatId), "Choose your daily reminder time:");
+
+        List<KeyboardRow> rows = new ArrayList<>();
+        KeyboardRow currentRow = new KeyboardRow();
+
+        for (int hour = 0; hour < 24; hour++) {
+            for (int minute : new int[]{0, 30}) {
+
+                String time = String.format("%02d:%02d", hour, minute);
+                currentRow.add(new KeyboardButton(time));
+
+                if (currentRow.size() == 4) {
+                    rows.add(currentRow);
+                    currentRow = new KeyboardRow();
+                }
+            }
+        }
+
+        if (!currentRow.isEmpty()) {
+            rows.add(currentRow);
+        }
+
+        ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup(rows);
+        keyboard.setResizeKeyboard(true);
+        keyboard.setOneTimeKeyboard(true);
+
+        sendMessage.setReplyMarkup(keyboard);
+
+        return sendMessage;
     }
 }
